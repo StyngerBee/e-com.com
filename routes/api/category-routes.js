cb5647918042305bc09030c1c1baa3dfc3b8700c
Curran Duke <cdsonics@yahoo.com>
2022-11-07 18:21:39 -0500

    added router.post for a new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,9 +43,16 @@ router.get('/:id', (req, res) => {
   });
 });
 
-router.post('/', (req, res) => {
-  // create a new category
-  
+router.post('/', async (req, res) => {
+  // creates a new category
+  try {
+    const newCategory = await Category.create({
+      category_name: req.body.category_name
+    });
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
