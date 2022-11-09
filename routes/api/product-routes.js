a0e480f8ccfbd204d2c5dd19a3f11980d47728c4
Curran Duke <cdsonics@yahoo.com>
2022-11-08 19:08:44 -0500

    added delete request for id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -105,8 +105,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+ try { 
+  const deleteProd = await Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  if (!deleteProd) {
+    res.status(400).json(err);
+  }
+  res.status(200).json(deleteProd);
+ } catch (err) {
+  res.status (500).json(err);
+ }
 });
 
 module.exports = router;
