16a96bade30a490fba5430c5fce3c3ec0ccc6ce4
Curran Duke <cdsonics@yahoo.com>
2022-11-08 19:18:45 -0500

    added get request for single  id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,37 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  // finds all tags
+  try {
+    const tagData = await Tag.findAll(
+      {
+        // includes its associated Product data
+        include: [{ model: Product, through: ProductTag}]
+      }
+    );
+    res.json({ tagData});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  // finds a single tag by its `id`
+  try {
+    const atagData = await Tag.findByPk( req.params.id,
+      { //includes its associated Product data
+        include: [{model: Product, through: ProductTag}]
+      });
+    if (!atagData){
+      res.status(400).json(err);
+    }
+    res.json({atagData});
+  } catch (err) {
+    res.status(500).json(err)
+  }
+  
 });
 
 router.post('/', (req, res) => {
